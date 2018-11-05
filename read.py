0139514e5e1e1fc16aac4fd28512ec8749555abf
qaswedf <qaswedf1630@gmail.com>
2018-11-05 14:49:11 +0800

    add file

--- read.py
+++ read.py
@@ -3,8 +3,16 @@ with open ('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 
-sum_len = 0
+wc = {}
 for d in data:
-	sum_len = len(d) + sum_len
-avg = sum_len / len(data)
-print(avg)
+	words = d.split(" ")
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+# sum_len = 0
+# for d in data:
+# 	sum_len = len(d) + sum_len
+# avg = sum_len / len(data)
+# print(avg)
